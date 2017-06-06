ce439a008bbf6c18673c5e6fed57f7ccbc8fe285
Doublemine <doublemine.w@gmail.com>
2017-06-06 18:10:27 +0800

    custom admin with choice tab

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,26 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
 
 # Register your models here.
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # This isn’t impressive with only two fields, but for admin forms with dozens of fields, choosing an intuitive
+    # order is an important usability detail.
+    # fields = ['pub_date', 'question_text']
+
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
