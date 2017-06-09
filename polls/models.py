f1a25dd6fbcb352ecfa26477ed609322a6713128
Doublemine <doublemine.w@gmail.com>
2017-06-09 15:10:37 +0800

    add custom objects manager

--- polls/models.py
+++ polls/models.py
@@ -19,10 +19,18 @@ class Question(models.Model):
     was_published_recently.short_description = "是否最近发布"
 
 
+class ChoiceManager(models.Manager):
+    def test_filter(self, test):
+        # 这个方法使用了self.filter()，此处self指manager本身
+        return self.filter(choice_text__contains=test).count()
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    #  它将取代模型的默认manager（objects）如果我们没有特别定义，它将会被自动创建。 我们把它命名为objects，这是为了与自动创建的manager保持一致。
+    objects = ChoiceManager()
 
     def __str__(self):
         return self.choice_text
